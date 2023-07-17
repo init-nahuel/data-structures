d84c47f387353368f5b1e7e2a1bf008c0ebe1cd9
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-07-17 17:59:30 -0400

    feat: removeNode fn added

--- linked_lists/c/linkedList.c
+++ linked_lists/c/linkedList.c
@@ -9,7 +9,7 @@ typedef struct {
  * Add a new node with a value to the front of a linked list
  * ---------------------------------------------------------
  * val: value for the new node to add
- * linkedList: linked list where the new node will be added
+ * head: linked list where the new node will be added
  * 
  * returns: The linkedList modified with the new value added
  */
@@ -23,3 +23,35 @@ Node* addNode(int val, Node* head) {
     return head;
 }
 
+/**
+ * Remove the node with the value val from a linked list
+ * --------------------------------------------------------
+ * val: value to be removed from the list
+ * head: linked list from where the node will be removed
+ * 
+ * returns: 0 if the value was removed from the ll and 1 if was not found
+*/
+int removeNode(int val, Node** linkedList) {
+    Node* head = *linkedList; 
+    Node* current = head;
+    Node* prev = head;
+
+    while (current != NULL) {
+        if (current->val == val) {
+            if (current == head) {
+                head = current->next;
+            } else {
+                prev->next = current->next;
+            }
+            
+            free(current);
+            current = NULL;
+            return 1;
+        }
+
+        prev = current;
+        current = current->next;
+    }
+    
+    return 0;
+}
